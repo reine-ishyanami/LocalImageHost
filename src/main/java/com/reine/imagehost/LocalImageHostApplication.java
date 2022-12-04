679e48189197936bc2d7400868f647a36af864c7
reine-ishyanami <2402979195@qq.com>
2022-12-04 20:27:03 +0800

    修改启动方式

--- src/main/java/com/reine/imagehost/LocalImageHostApplication.java
+++ src/main/java/com/reine/imagehost/LocalImageHostApplication.java
@@ -6,15 +6,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 @Slf4j
 public class LocalImageHostApplication extends AbstractJavaFxApplicationSupport {
 
     public static void main(String[] args) {
-        if (args.length != 0 && "GUI".equals(args[0])) {
+        System.out.println("是否启动图形界面y/n（默认n）: ");
+        Scanner scanner = new Scanner(System.in);
+        String isGUI = scanner.nextLine();
+        if ("y".equals(isGUI)){
             log.info("Running in GUI mode");
             launch(LocalImageHostApplication.class, MainView.class, args);
-        } else {
+        }else {
             log.info("Running in Terminal mode");
             SpringApplication.run(LocalImageHostApplication.class, args);
         }
