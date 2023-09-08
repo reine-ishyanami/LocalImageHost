da807f68a36fbbd4d334151e9a7e9f3677b332f1
reine-ishyanami <2402979195@qq.com>
2023-09-08 10:27:15 +0800

    指定启动参数启动

--- src/main/java/com/reine/imagehost/LocalImageHostApplication.java
+++ src/main/java/com/reine/imagehost/LocalImageHostApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -12,24 +13,33 @@ import java.util.Scanner;
 public class LocalImageHostApplication {
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("是否启动图形界面y/n（默认n）: ");
-            inputLoop:
-            while (true) {
-                switch (scanner.nextLine()) {
-                    case "", "n", "N" -> {
-                        log.info("Running in Terminal mode");
-                        SpringApplication.run(LocalImageHostApplication.class, args);
-                        break inputLoop;
+        String mode = System.getProperty("mode");
+        Optional.ofNullable(mode).ifPresentOrElse((m) -> {
+            switch (m.toLowerCase()) {
+                case "terminal", "shell" -> SpringApplication.run(LocalImageHostApplication.class, args);
+                case "gui" -> AppUI.main(args);
+                default -> log.error("参数有误，请检查启动参数");
+            }
+        }, () -> {
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("是否启动图形界面y/n（默认n）: ");
+                inputLoop:
+                while (true) {
+                    switch (scanner.nextLine()) {
+                        case "", "n", "N" -> {
+                            log.info("Running in Terminal mode");
+                            SpringApplication.run(LocalImageHostApplication.class, args);
+                            break inputLoop;
+                        }
+                        case "y", "Y" -> {
+                            log.info("Running in GUI mode");
+                            AppUI.main(args);
+                            break inputLoop;
+                        }
+                        default -> System.out.print("输入有误，请重新选择是否启动图形界面y/n（默认n）: ");
                     }
-                    case "y", "Y" -> {
-                        log.info("Running in GUI mode");
-                        AppUI.main(new String[]{});
-                        break inputLoop;
-                    }
-                    default -> System.out.print("输入有误，请重新选择是否启动图形界面y/n（默认n）: ");
                 }
             }
-        }
+        });
     }
 }
